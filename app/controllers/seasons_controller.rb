c702b741b02262a5515b9620bd23ec61f78d0928
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-10 21:11:49 -0500

    Add list all seasons in indx.html.erb

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,4 +2,22 @@ class SeasonsController < ApplicationController
   def index
     @seasons = Season.all
   end
+
+  def new
+  end
+
+  def create
+    if params[:season][:fema_state_emg] == true
+      fema_state_emg = true
+    else
+      fema_state_emg = false
+    end
+    season = Season.new({
+      year: params[:season][:year].to_i,
+      biggest_storm: params[:season][:biggest_storm],
+      fema_state_emg: fema_state_emg
+    })
+    season.save
+    redirect_to '/seasons'
+  end
 end
