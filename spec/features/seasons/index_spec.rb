4b0e3e06abfeba8d148717386a9ec4e92425a415
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-12 15:02:15 -0500

    Add feature tests for show

---
+++ spec/features/seasons/index_spec.rb
@@ -0,0 +1,19 @@
+# require 'rails_helper'
+
+# RSpec.describe 'On the tested page' do
+#   # As a visitor
+#   # When I visit /URI
+#   # Then I see
+#   describe 'As a visitor' do
+#     describe 'When I visit /URI' do
+#       it 'Then I see' do
+#         model = Model.create!(column: "Value", [...])
+#         model_2 = model_many.model_belong.create!(column: "Value", [...])
+#         visit “URI/#{model.id}”
+#         Save_and_open_page # requires launchy gem
+#         expect(page).to have_content(model.column)
+#         expect(page).to_not have_content("Thing #{model_2.column}")
+#       end
+#     end
+#   end
+# end
