0411c27e4419f7a04fcdee9e317e628bead0f00a
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-13 21:21:57 -0500

    Add tests for season counter

--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -3,9 +3,18 @@ require 'rails_helper'
 RSpec.describe Season, type: :model do
   it { should have_many :storms }
 
-  # describe 'In Season Model' do
-  #   describe '#method' do
-  #     it 'Will...'
-  #   end
-  # end
+  describe 'In Season Model' do
+     describe '#storms_counter' do
+      it 'Will count the number of storms' do
+        season_21 = Season.create!(year: 2021, biggest_storm: "Ian", fema_state_emg: true)
+        season_21.storms.create!(storm_type: "Hurricane", landfall: false, wind_spd: 155, name: "Sam")
+        season_21.storms.create!(storm_type: "Hurricane", landfall: true, wind_spd: 154, name: "Ian")
+        season_17 = Season.create!(year: 2017, biggest_storm: "Irma", fema_state_emg: true)
+        season_17.storms.create!(storm_type: "Hurricane", landfall: false, wind_spd: 180, name: "Irma")
+
+        expect(season_21.storms_counter).to eq(2)
+        expect(season_17.storms_counter).to eq(1)
+      end
+    end
+  end
 end
