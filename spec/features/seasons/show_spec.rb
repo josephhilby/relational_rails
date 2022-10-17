edc3a71823b8cefed0bcac03869146ff0cf86dc5
Joseph Hilby <joseph.hilby@gmail.com>
2022-10-17 16:02:13 -0500

    Fix tests in show to not give false neg

--- spec/features/seasons/show_spec.rb
+++ spec/features/seasons/show_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Seasons Show Page' do
-  describe 'When I visit /seasons/:id' do
-    before(:each) do
+  before(:each) do
       @season_22 = Season.create!(year: 2022, biggest_storm: "Ian", fema_state_emg: true)
-      @season_22.storms.create!(storm_type: "Hurricane", landfall: false, wind_spd: 180, name: "Andrew")
       @season_21 = Season.create!(year: 2021, biggest_storm: "Sam", fema_state_emg: true)
-      @season_21.storms.create!(storm_type: "Hurricane", landfall: false, wind_spd: 155, name: "Sam")
-      @season_21.storms.create!(storm_type: "Hurricane", landfall: true, wind_spd: 154, name: "Ian")
+      @storm_andrew = @season_22.storms.create!(storm_type: "Hurricane", landfall: true, wind_spd: 180, name: "Andrew")
+
+      @storm_sam = @season_21.storms.create!(storm_type: "Hurricane", landfall: false, wind_spd: 155, name: "Sam")
+      @storm_ian = @season_21.storms.create!(storm_type: "Hurricane", landfall: true, wind_spd: 154, name: "Ian")
     end
+  describe 'When I visit /seasons/:id' do
     describe 'Then I see' do
       it '1) The season with that id including its attributes' do
         visit "/seasons/#{@season_22.id}"
@@ -60,7 +61,7 @@ RSpec.describe 'Seasons Show Page' do
       it '1) The season is deleted' do
         visit "/seasons/#{@season_21.id}"
         click_on "Delete"
-        save_and_open_page
+
         expect(current_path).to eq("/seasons")
         expect(page).to_not have_content("2021")
       end
@@ -69,9 +70,16 @@ RSpec.describe 'Seasons Show Page' do
         visit "/seasons/#{@season_21.id}"
         click_on "Delete"
         visit "/storms"
+
+        expect(page).to_not have_content("#{@storm_ian[:name]}")
+        expect(page).to_not have_content("#{@storm_sam[:name]}")
+      end
+
+      it 'test' do
+        visit "/seasons"
         save_and_open_page
-        expect(page).to_not have_content("Sam")
-        expect(page).to_not have_content("Ian")
+        visit "/storms"
+        save_and_open_page
       end
     end
   end
